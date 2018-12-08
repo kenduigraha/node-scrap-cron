a1040e8d4d416998029041582a22ecf2c79a7159
Ken Duigraha Putra <kenduigraha@gmail.com>
2018-12-08 23:56:55 +0700

    Add unit testing for healthcheck, install all packages for unit testing.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
+require('dotenv').config()
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
